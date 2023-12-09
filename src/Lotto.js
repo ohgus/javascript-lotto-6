9388eaa9126d2a946c25cb27aea04c12acbd5f3b
ohgus <anakin1108@naver.com>
2023-12-09 19:52:01 +0900

    feat: 당첨 정보 저장하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,45 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, customerLottos) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.customerLottos = customerLottos;
+  }
+
+  getRankInfo(bonusNumber) {
+    const rankInfo = Array.from({ length: 5 }, () => 0);
+
+    return this.#calculateRank(bonusNumber, rankInfo);
+  }
+
+  #calculateRank(bonusNumber, rankInfo) {
+    this.customerLottos.forEach((lotto) => {
+      const matchCount = this.#countMatch(lotto, this.#numbers);
+      const haveBonus = this.#checkBonus(lotto, bonusNumber);
+
+      if (matchCount === 3) return (rankInfo[4] += 1);
+      if (matchCount === 4) return (rankInfo[3] += 1);
+      if (matchCount === 5 && !haveBonus) return (rankInfo[2] += 1);
+      if (matchCount === 5 && haveBonus) return (rankInfo[1] += 1);
+      if (matchCount === 6) return (rankInfo[0] += 1);
+    });
+
+    return rankInfo;
+  }
+
+  #countMatch(customerLotto, winningNumbers) {
+    let match = 0;
+
+    customerLotto.forEach((number) => {
+      if (winningNumbers.includes(number)) match += 1;
+    });
+
+    return match;
+  }
+
+  #checkBonus(lotto, bonusNumber) {
+    return lotto.includes(Number(bonusNumber));
   }
 
   #validate(numbers) {
@@ -11,8 +47,9 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
+
+const lotto = new Lotto([1, 2, 3, 4, 5, 6], [[1, 2, 3, 4, 5, 6]]);
+console.log(lotto.getRankInfo(7));
