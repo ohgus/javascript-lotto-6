c4be2cd37b7e1910f1111506e417c13ab1ca1f0f
ohgus <anakin1108@naver.com>
2023-12-14 17:01:54 +0900

    feat: 구입금액 예외처리 추가

---
+++ src/model/Store.js
@@ -0,0 +1,23 @@
+import { ERROR } from "../constants/messages.js";
+import { LOTTO_OPTIONS } from "../constants/lottoOptions.js";
+
+class Store {
+  #coins;
+
+  constructor(input) {
+    this.#validate(input);
+    this.#coins = Number(input);
+  }
+
+  #validate(input) {
+    const coins = Number(input);
+
+    if (!LOTTO_OPTIONS.coinRegex.test(coins)) {
+      throw new Error(`${ERROR.title} ${ERROR.message.NaN}`);
+    } else if (coins < LOTTO_OPTIONS.coinUnit) {
+      throw new Error(`${ERROR.title} ${ERROR.message.small}`);
+    } else if (coins % LOTTO_OPTIONS.coinUnit !== 0) {
+      throw new Error(`${ERROR.title} ${ERROR.message.unit}`);
+    }
+  }
+}
