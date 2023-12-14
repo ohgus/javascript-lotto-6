76bf5aa048bfd4c3a2eb5aa803f3186fd2ebf1b4
ohgus <anakin1108@naver.com>
2023-12-14 17:15:10 +0900

    feat: 보너스번호 예외처리 추가

---
+++ src/model/Bonus.js
@@ -0,0 +1,23 @@
+import { LOTTO_OPTIONS } from "../constants/lottoOptions.js";
+import { ERROR } from "../constants/messages.js";
+
+class Bonus {
+  #bonusNumber;
+
+  constructor(input, lottoNumbers) {
+    this.#validate(input, lottoNumbers);
+    this.#bonusNumber = Number(input);
+  }
+
+  #validate(input, lottoNumbers) {
+    const bonusNumber = Number(input);
+
+    if (!LOTTO_OPTIONS.regex.test(bonusNumber)) {
+      throw new Error(`${ERROR.title} ${ERROR.message.range}`);
+    } else if (lottoNumbers.includes(bonusNumber)) {
+      throw new Error(`${ERROR.title} ${ERROR.message.duplicate}`);
+    }
+  }
+}
+
+export default Bonus;
